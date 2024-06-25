6f811932aabc8b534ecbe1d489efba41616f4b3d
YoungKyonYou <man.spride1324@gmail.com>
2024-06-25 23:35:39 +0900

    chore: 수정된 폴더 경로 적용

---
+++ src/test/java/com/score/tracker/ScoreTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.score.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
